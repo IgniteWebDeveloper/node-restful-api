ebb8e74cdf1751078500087ffcebd1da5b6c4b4d
Syed Adnan Ali <61422258+IgniteWebDeveloper@users.noreply.github.com>
2022-04-16 09:22:32 +0530

    first commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
-
+    
 app.use("/api/v1/user", userRoutes);
 
 // use error middleware
